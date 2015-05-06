8eefac3c101037bb7b4f79dde2195ff45088b828
anais <anaiskat@gmail.com>
2015-05-06 11:09:46 +0000

    hidding an empty cart, making images clickable, testing ajax changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def hidden_div_if(condition, attributes = {}, &block)
+    
+    if condition
+      attributes["style"] = "display: none"
+    end
+    
+    #content_tag is a helper that pass the output from the &block above in the hidden_dib method
+    content_tag("div", attributes, &block)
+  end  
+  
 end
